44dcce534edccadfcac6354925bb8ea1f36db732
Carson <czt0023@auburn.edu>
2018-12-01 14:29:41 -0600

    Can compile both in 'files that matter'

--- files_that_matter/sender.c
+++ files_that_matter/sender.c
@@ -16,12 +16,12 @@
 
 typedef struct Element {
   char current_element[25];
-  Element next_element;
+  struct Element * next_element;
 }Element_t;
 
 typedef struct Queue {
   int queue_len;
-  Element element_queue;
+  Element_t element_queue;
 }Queue_t;
 
 /* The sender function will conntinuously send packets to the server
@@ -29,8 +29,8 @@ typedef struct Queue {
 *  add the timestamp for fin_time and the queue length to the packet
 *  before sending */
 
-void sender(Queue *queue) {
-
+void *sender(Queue_t *queue) {
+  return 0;
 }
 
 /* The main function will create two queues, queue_1 and queue_2. These will
@@ -40,15 +40,15 @@ void sender(Queue *queue) {
 *  into the queue */
 
 int main() {
-  Queue queue_1;
-  Queue queue_2;
-  pthread thread_1;
-  pthread thread_2;
+  Queue_t queue_1;
+  Queue_t queue_2;
+  pthread_t thread_1;
+  pthread_t thread_2;
 
-  pthread_create(&thread_1, NULL, sender(queue_1), NULL);
-  pthread_create(&thread_2, NULL, sender(queue_2), NULL);
+  pthread_create(&thread_1, NULL, sender(&queue_1), NULL);
+  pthread_create(&thread_2, NULL, sender(&queue_2), NULL);
 
-  for (i = 0; i <= 10000; i++) {
+  for (int i = 0; i <= 10000; i++) {
     // add a new packet to a queue
   }
   return 0;
