c3ec571861a3b87b6d8f17b56bfa6e1d574afd1a
Carson <czt0023@auburn.edu>
2018-12-05 17:45:08 -0600

    Updated sender.c to have no multi-threading in mind

--- files_that_matter/sender.c
+++ files_that_matter/sender.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -9,25 +10,23 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <pthread.h>
+#include <sys/queue.h>
 
-#define PORT_1 "10010"
-#define PORT_2 "10011"
+#define PORT "10010"
 
 #define ADDRESS "131.204.2.9"
 
-/* The queue structure will hold the data for the queues. It is implemented
-*  using a linked list */
+/* Defines the number of packets loaded at a time. Change this to
+*  alter the arrival rate and test performance metrics */
+const int ARRIVAL_RATE = 5;
 
-typedef struct Element {
-  char current_element[25];
-  struct Element * next_element;
-}Element_t;
-
-typedef struct Queue {
-  int queue_len;
-  Element_t element_queue;
-}Queue_t;
+/* Defines the number of packets that exit each queue. Packets
+*  leave the queue after ARRIVAL_RATE number of packets have been loaded,
+*  and SERVICE_RATE number of packets is sent from each queue with each
+* send process. */
+const int SERVICE_RATE = 1;
+const int array_x = 10000;
+const int array_y = 25;
 
 /* The sender function will conntinuously send packets to the server
 *  from the queue. It must increment decrement the queue length, and
@@ -36,49 +35,41 @@ typedef struct Queue {
 
 void *get_in_addr(struct sockaddr *sa)
 {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
 
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-void *sender(Queue_t *queue, int port) {
+int sender(time_t *time_array) {
 
-  int sockfd, numbytes;
-  struct addrinfo hints, *servinfo, *p;
-  int rv;
-  char s[INET6_ADDRSTRLEN];
+	int sockfd, numbytes;
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	char s[INET6_ADDRSTRLEN];
 
-  char inbuf[25];
-  char rcvbuf[25];
+	char inbuf[25];
+	char rcvbuf[25];
 
-  int counter;
-  char firstNum[11];
-  char secondNum[11];
-  int foundOp = 0;
+	int counter;
+	char firstNum[11];
+	char secondNum[11];
+	int foundOp = 0;
 
-  char operand;
-  unsigned int firstInt, secondInt;
+	char operand;
+	unsigned int firstInt, secondInt;
 
-  unsigned int rcvFirstInt, rcvSecondInt, rcvResult;
-  char rcvOperand, flag;
+	unsigned int rcvFirstInt, rcvSecondInt, rcvResult;
+	char rcvOperand, flag;
 
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
 
-  if (port == 1) {
-    if ((rv = getaddrinfo(ADDRESS, PORT_1, &hints, &servinfo)) != 0) {
-      fprintf(stderr, "Client: getaddrinfo: %s\n", gai_strerror(rv));
-      return 1;
-    }
-  }
-  if (port == 2) {
-    if ((rv = getaddrinfo(ADDRESS, PORT_2, &hints, &servinfo)) != 0) {
-      fprintf(stderr, "Client: getaddrinfo: %s\n", gai_strerror(rv));
-      return 1;
-    }
+	if ((rv = getaddrinfo(ADDRESS, PORT, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "Client: getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
   }
 
   // loop through all the results and connect to the first we can
@@ -110,10 +101,8 @@ void *sender(Queue_t *queue, int port) {
     freeaddrinfo(servinfo); // all done with this structure
 
     char outbuf[25];
-    int i;
-    for (i = 0; i < 25; i++) {
-      outbuf[i] = 'x';
-    }
+    outbuf[0] = 'x';
+
     if ((numbytes = send(sockfd, outbuf, 25, 0)) == -1) {
       perror("Line 107");
       exit(1);
@@ -121,31 +110,74 @@ void *sender(Queue_t *queue, int port) {
       printf("Client: sent to server");
     }
 
+    printf("%s\n", outbuf);
 
 
     return 0;
   }
 
-  /* The main function will create two queues, queue_1 and queue_2. These will
-  *  be put onto seperate threads and run concurrently. The main function will
+  /* The main function will create two queues, queue_1 and queue_2.
+  *  The main function will
   *  then add new packets to each queue, based on either a minimum queue or RNG
   *  queueing system. the packets loaded must contain an initial time of arrival
   *  into the queue */
 
   int main() {
-    Queue_t queue_1;
-    Queue_t queue_2;
-    pthread_t thread_1;
-    pthread_t thread_2;
-    int port_1 = 1;
-    int port_2 = 2;
+    time_t queue_1[10000];
+    time_t queue_2[10000];
+    int queue_1_len = 0;
+    int queue_2_len = 0;
 
-    //   pthread_create(&thread_1, NULL, sender(&queue_1, port_1), NULL);
-    //   pthread_create(&thread_2, NULL, sender(&queue_2, port_2), NULL);
-    sender(&queue_1, port_1);
 
-    for (int i = 0; i <= 10000; i++) {
-      // add a new packet to a queue
+    /* queue_method denotes the method taken when laoding the queue:
+    *	 0 = minimum queue
+    *  1 = random queue */
+    int queue_method = 0;
+    int rand_queue = 0;
+
+    srand(time(0));
+    int i = 0;
+    for (i; i < 5; i++) {
+
+      int j = 0;
+      for (j; j < ARRIVAL_RATE; j++) {
+
+        time_t start_time = time(NULL);
+        switch (queue_method) {
+          // Minimum queue model
+          case 0:
+          if (queue_1_len < queue_2_len) {
+            queue_1[queue_1_len + 1] = start_time;
+            queue_1_len++;
+          }
+          if (queue_1_len > queue_2_len) {
+            queue_2[queue_2_len + 1] = start_time;
+            queue_2_len++;
+          }
+          else {
+            queue_1[queue_1_len + 1] = start_time;
+            queue_1_len++;
+          }
+          break;
+
+          // Random queue model
+          case 1:
+          rand_queue = rand() % 2;
+          if (rand_queue == 0) {
+            queue_1[queue_1_len + 1] = start_time;
+            queue_1_len++;
+          }
+          else {
+            queue_2[queue_2_len + 1] = start_time;
+            queue_2_len++;
+          }
+          break;
+        }
+      }
+
+      sender(queue_1);
+      sender(queue_2);
     }
+
     return 0;
   }
