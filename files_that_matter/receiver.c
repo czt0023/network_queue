0e58edf46fc0e10fdbcd3549ba86776d485ff21d
Carson <czt0023@auburn.edu>
2018-12-07 20:13:58 -0600

    Receiver final

--- files_that_matter/receiver.c
+++ files_that_matter/receiver.c
@@ -1,15 +1,4 @@
-/* This will run on the reveiving server. It will take in the packets sent
-*  by the queues. It will store each packet in a vector and, after it has
-*  received the last packet, it will calculate the following:
-*    average queue length
-*    average time in queues
-*    number of dropped packets
-*/
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <stdio.h>
 #include<string.h>
 
 int main()
@@ -17,10 +6,7 @@ int main()
 
   char str[25];
   int listen_fd, comm_fd;
-  char end_string[25];
-  for (int i = 0; i < 25; i++) {
-    end_string[i] = 'x';
-  }
+  int i = 0;
 
   struct sockaddr_in servaddr;
 
@@ -37,56 +23,24 @@ int main()
   listen(listen_fd, 10);
 
   comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
+  int packets_received;
 
-  while(1)
-  {
+  while (1) {
     bzero( str, 25);
 
     read(comm_fd,str,25);
-    if (end_string == str) {
-      printf("Final packet received\n");
-      continue;
+    if ('x' == str[0]) {
+      printf("Shutdown packet received\n");
+      break;
     }
 
     printf("Recieved new packet\n");
+    printf("%s\n", str);
+    packets_received++;
   }
 
+
   close(listen_fd);
+  printf("%d packets received", packets_received);
 
-}
-
-//Calculates the average length of time the packets were in the queue. 
-double avgWTime()
-{
-    double timeDiff;
-    int totalPacks;
-    double time;
-    double timeAvg = 0;
-    
-    timeAvg = time/totalPacks;
-    return timeAvg;
-}
-
-//Calculates the average length of the queue
-double avgQLen()
-{
-    double sizeofQ;
-    double avgLen = 0;
-    double arrivalTime;
-    double serviceTime;
-    double roe = arrivalTime/serviceTime;
-    
-    avgLength = (roe/(1 - roe));
-    return avgLen; 
-}
-
-//Calculate the probability a packet being dropped during the process
-double blockProb()
-{
-    int totalPacks;
-    int dropPacks;
-    double packProb;
-    
-    packpProb = dropPacks/totalPacks;
-    return packProb;
 }
